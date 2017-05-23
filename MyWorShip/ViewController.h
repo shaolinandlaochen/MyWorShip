a0872f85f6472e5ff7c4210e042ffcad317f7d84
123 <a123@123deMac-mini.local>
2017-05-23 15:26:57 +0800

    Initial Commit

---
+++ MyWorShip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWorShip
+//
+//  Created by 123 on 2017/5/23.
+//  Copyright © 2017年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyWorShip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyWorShip
+//
+//  Created by 123 on 2017/5/23.
+//  Copyright © 2017年 123. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
