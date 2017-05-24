b3c3a938adb59efa2c3ef0b17126f1c464f75f0c
123 <a123@123deMac-mini.local>
2017-05-24 14:29:36 +0800

    扫描原生

--- MyWorShip/ScanningViewController.h
+++ MyWorShip/ScanningViewController.h
@@ -9,7 +9,13 @@
 #import "MyClass.h"
 
 @interface ScanningViewController : MyClass
-@property (nonatomic,strong) UIImageView *centerView;
-@property (nonatomic,strong) ZXCapture *capture;
+@property (strong, nonatomic) UIView *boxView;
+@property (nonatomic) BOOL isReading;
+@property (strong, nonatomic) CALayer *scanLayer;
+
+//捕捉会话
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+//展示layer
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *videoPreviewLayer;
 
 @end
--- MyWorShip/ScanningViewController.m
+++ MyWorShip/ScanningViewController.m
@@ -8,39 +8,123 @@
 
 #import "ScanningViewController.h"
 
-@interface ScanningViewController ()<ZXCaptureDelegate>
-
+@interface ScanningViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+{
+    UIView *_boxView;
+}
 @end
 
 @implementation ScanningViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor=[self colorWithHexString:@"f3f5f7"];
-    self.centerView=[[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
-    //[self.centerView setImage:[UIImage imageNamed:@"shape_fold"]];
-    self.centerView.backgroundColor=[UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.2];
-    self.centerView.center=self.view.center;
-    [self.view addSubview:self.centerView];
-    
-    self.capture = [[ZXCapture alloc] init];
-    self.capture.camera = self.capture.back;
-    self.capture.focusMode = AVCaptureFocusModeContinuousAutoFocus;
-    self.capture.rotation = 90.0f;
-    self.capture.layer.frame = self.view.bounds;
-    [self.view.layer addSublayer:self.capture.layer];
-    
-    [self.view bringSubviewToFront:self.centerView];
+    self.view.backgroundColor=[[UIColor blackColor]colorWithAlphaComponent:0.2];
+    _captureSession = nil;
+    _isReading = NO;
+    self.title=@"扫描二维码";
+    [self startReading];
     // Do any additional setup after loading the view.
 }
+//开始
+-(BOOL)startReading
+{
+    //1.初始化捕捉设备（AVCaptureDevice），类型为AVMediaTypeVideo
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error =nil;
+    //2.用captureDevice创建输入流
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    if (!input) {
+        NSLog(@"%@", [error localizedDescription]);
+        return NO;
+    }
+    
+    //3.创建媒体数据输出流
+    AVCaptureMetadataOutput *captureMetadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    //4.实例化捕捉会话
+    _captureSession = [[AVCaptureSession alloc] init];
+    //4.1.将输入流添加到会话
+    [_captureSession addInput:input];
+    //4.2.将媒体输出流添加到会话中
+    [_captureSession addOutput:captureMetadataOutput];
+    //5.创建串行队列，并加媒体输出流添加到队列当中
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    //5.1.设置代理
+    [captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    //5.2.设置输出媒体数据类型为QRCode
+    //类比系统地图的类型去学习
+    [captureMetadataOutput setMetadataObjectTypes:[NSArray arrayWithObject:AVMetadataObjectTypeQRCode]];
+    
+    //6.实例化预览图层
+    _videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    //7.设置预览图层填充方式
+    [_videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    //8.设置图层的frame
+    [_videoPreviewLayer setFrame:self.view.layer.bounds];
+    //9.将图层添加到预览view的图层上
+    [self.view.layer addSublayer:_videoPreviewLayer];
+    
+    //10.设置扫描范围
+    captureMetadataOutput.rectOfInterest = CGRectMake(0.2f, 0.2f, 0.8f, 0.8f);
+    
+    //10.1.扫描框
+    _boxView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 200, 200)];
+    _boxView.layer.borderColor = [UIColor greenColor].CGColor;
+    _boxView.layer.borderWidth = 1.0f;
+    [self.view addSubview:_boxView];
+    //10.2.扫描线
+    _scanLayer = [[CALayer alloc] init];
+    _scanLayer.frame = CGRectMake(0, 0, _boxView.bounds.size.width, 1);
+    _scanLayer.backgroundColor = [UIColor brownColor].CGColor;
+    [_boxView.layer addSublayer:_scanLayer];
+    
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:0.2f target:self selector:@selector(moveScanLayer:) userInfo:nil repeats:YES];
+    [timer fire];
+    
+    
+    //10.开始扫描
+    [_captureSession startRunning];
+    
+    
+    return YES;
+}
+- (void)moveScanLayer:(NSTimer *)timer
+{
+    CGRect frame = _scanLayer.frame;
+    if (_boxView.frame.size.height < _scanLayer.frame.origin.y) {
+        frame.origin.y = 0;
+        _scanLayer.frame = frame;
+    }else{
+        frame.origin.y += 10;
+        [UIView animateWithDuration:0.1 animations:^{
+            _scanLayer.frame = frame;
+        }];
+    }
+}
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    //判断是否有数据
+    if (metadataObjects != nil && [metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        NSLog(@"扫描结果===%@",metadataObj.stringValue);
+        //判断回传的数据类型
+        if ([[metadataObj type] isEqualToString:AVMetadataObjectTypeQRCode]) {
+          //  [_lblStatus performSelectorOnMainThread:@selector(setText:) withObject:[metadataObj stringValue] waitUntilDone:NO];
+            [self performSelectorOnMainThread:@selector(stopReading) withObject:nil waitUntilDone:NO];
+            _isReading = NO;
+        }
+    }
+}
+//暂停
+-(void)stopReading{
+    [_captureSession stopRunning];
+    _captureSession = nil;
+    [_scanLayer removeFromSuperlayer];
+    [_videoPreviewLayer removeFromSuperlayer];
+}
+
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    self.navigationController.navigationBarHidden=YES;
-    self.capture.delegate = self;
-    self.capture.layer.frame = self.view.bounds;
-    
-    CGAffineTransform captureSizeTransform = CGAffineTransformMakeScale(320 / self.view.frame.size.width, 480 / self.view.frame.size.height);
-    self.capture.scanRect = CGRectApplyAffineTransform(self.centerView.frame, captureSizeTransform);
     
 }
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
@@ -50,48 +134,6 @@
     AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
 }];
 }
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
-    return toInterfaceOrientation == UIInterfaceOrientationPortrait;
-}
-
-- (NSString *)barcodeFormatToString:(ZXBarcodeFormat)format {
-    switch (format) {
-        case kBarcodeFormatAztec:
-            return @"Aztec";
-        case kBarcodeFormatCodabar:
-            return @"CODABAR";
-        case kBarcodeFormatCode39:
-            return @"Code 39";
-        case kBarcodeFormatCode93:
-            return @"Code 93";
-        case kBarcodeFormatCode128:
-            return @"Code 128";
-        case kBarcodeFormatDataMatrix:
-            return @"Data Matrix";
-        case kBarcodeFormatEan8:
-            return @"EAN-8";
-        case kBarcodeFormatEan13:
-            return @"EAN-13";
-        case kBarcodeFormatITF:
-            return @"ITF";
-        case kBarcodeFormatPDF417:
-            return @"PDF417";
-        case kBarcodeFormatQRCode:
-            return @"QR Code";
-        case kBarcodeFormatRSS14:
-            return @"RSS 14";
-        case kBarcodeFormatRSSExpanded:
-            return @"RSS Expanded";
-        case kBarcodeFormatUPCA:
-            return @"UPCA";
-        case kBarcodeFormatUPCE:
-            return @"UPCE";
-        case kBarcodeFormatUPCEANExtension:
-            return @"UPC/EAN extension";
-        default:
-            return @"Unknown";
-    }
-}
 
 
 - (void)didReceiveMemoryWarning {
@@ -99,17 +141,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)captureResult:(ZXCapture *)capture result:(ZXResult *)result {
-    if (!result){
-        return;
-    }
-    
-    NSString *formatString = [self barcodeFormatToString:result.barcodeFormat];
-    NSLog(@"%@",[NSString stringWithFormat:@"条码格式:%@ \n内容:%@", formatString, result.text]);
-    [self.capture stop];
-    //震动提示
-    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
-}
+
 
 
 /*
