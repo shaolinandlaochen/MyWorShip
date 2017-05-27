a52141f9b247ce1515669c24c68de7df6e78a6bd
123 <a123@123deMac-mini.local>
2017-05-27 17:20:01 +0800

    界面完事

--- MyWorShip/ViewController.m
+++ MyWorShip/ViewController.m
@@ -16,12 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if ([[NSUserDefaults standardUserDefaults]valueForKey:@"TheUser"]==nil) {
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        [defaults setObject:@"用户" forKey:@"TheUser"];
+        //同步数据
+        [defaults synchronize];
+        UIImageView *img=[[UIImageView alloc]init];
+        img.frame=self.view.bounds;
+        img.image=[UIImage imageNamed:@"initial_page"];
+        [self.view addSubview:img];
+        autoSize
+        UIButton *btn=[UIButton buttonWithType:UIButtonTypeCustom];
+        [btn addTarget:self action:@selector(onButtonClick) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:btn];
+        btn.sd_layout.leftSpaceToView(self.view, 30).rightSpaceToView(self.view, 30).bottomSpaceToView(self.view, 220*autoSizeScaleY).heightIs(120*autoSizeScaleY);
+    }else{
+        [self onButtonClick];//已经不是第一次进来了
+    }
+
     
-    [self CreateAView];
+   // [self CreateAView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
--(void)CreateAView{
+-(void)onButtonClick{
     HomeViewController *home=[[HomeViewController alloc]init];
     UINavigationController *nav=[[UINavigationController alloc]initWithRootViewController:home];
      [[UIApplication sharedApplication].delegate window].rootViewController=nav;
