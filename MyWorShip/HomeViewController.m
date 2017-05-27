268b1391fc47411f4826b28e8c1edf04536f5419
123 <a123@123deMac-mini.local>
2017-05-27 17:36:05 +0800

    哈哈哈明天端午节

--- MyWorShip/HomeViewController.m
+++ MyWorShip/HomeViewController.m
@@ -165,13 +165,61 @@
 }
 #pragma mark 扫描
 -(void)onScanningClick:(MyButton *)btn{
-    SGQRCodeScanningVC *Scanning =[[SGQRCodeScanningVC alloc]init];
-    Scanning.delegate=self;
-    UINavigationController *nav=[[UINavigationController alloc]initWithRootViewController:Scanning];
-    nav.modalPresentationStyle = UIModalPresentationOverFullScreen;
-    [self presentViewController:nav animated:YES completion:^{
-        //nav.view.backgroundColor = [[UIColor clearColor] colorWithAlphaComponent:0.5];
-    }];
+    // 1、 获取摄像设备
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device) {
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        if (status == AVAuthorizationStatusNotDetermined) {
+            [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                if (granted) {
+                    SGQRCodeScanningVC *Scanning =[[SGQRCodeScanningVC alloc]init];
+                    Scanning.delegate=self;
+                    UINavigationController *nav=[[UINavigationController alloc]initWithRootViewController:Scanning];
+                    nav.modalPresentationStyle = UIModalPresentationOverFullScreen;
+                    [self presentViewController:nav animated:YES completion:^{
+                        
+                    }];
+                    SGQRCodeLog(@"当前线程 - - %@", [NSThread currentThread]);
+                    // 用户第一次同意了访问相机权限
+                    SGQRCodeLog(@"用户第一次同意了访问相机权限");
+                    
+                } else {
+                    
+                    // 用户第一次拒绝了访问相机权限
+                    SGQRCodeLog(@"用户第一次拒绝了访问相机权限");
+                }
+            }];
+        } else if (status == AVAuthorizationStatusAuthorized) { // 用户允许当前应用访问相机
+            SGQRCodeScanningVC *vc = [[SGQRCodeScanningVC alloc] init];
+            [self.navigationController pushViewController:vc animated:YES];
+        } else if (status == AVAuthorizationStatusDenied) { // 用户拒绝当前应用访问相机
+            UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"⚠️ 警告" message:@"请去-> [设置 - 隐私 - 相机 - 奇拜] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+            UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                
+            }];
+            
+            [alertC addAction:alertA];
+            [self presentViewController:alertC animated:YES completion:nil];
+            
+        } else if (status == AVAuthorizationStatusRestricted) {
+            NSLog(@"因为系统原因, 无法访问相册");
+        }
+    } else {
+        UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"未检测到您的摄像头" preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        
+        [alertC addAction:alertA];
+        [self presentViewController:alertC animated:YES completion:nil];
+    }
+//    SGQRCodeScanningVC *Scanning =[[SGQRCodeScanningVC alloc]init];
+//    Scanning.delegate=self;
+//    UINavigationController *nav=[[UINavigationController alloc]initWithRootViewController:Scanning];
+//    nav.modalPresentationStyle = UIModalPresentationOverFullScreen;
+//    [self presentViewController:nav animated:YES completion:^{
+//        //nav.view.backgroundColor = [[UIColor clearColor] colorWithAlphaComponent:0.5];
+//    }];
 }
 #pragma  mark 添加 地图
 -(void)AddAMap{
